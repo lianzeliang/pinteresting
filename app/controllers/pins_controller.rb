2906cc58b494aae810f4f4e90273d9b7b4123c3c
lianzeliang <lianzeliang@qq.com>
2013-12-31 00:05:23 +0800

    order change

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,8 +2,8 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  def index
-    @pins = Pin.all
+   def index
+   @pins = Pin.all.order("created_at DESC")
   end
 
   def show
