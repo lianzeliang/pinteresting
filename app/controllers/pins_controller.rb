941d7ba11c3114ddaa238b3f71e09e8e828e9974
lianzeliang <lianzeliang@qq.com>
2014-01-01 22:11:09 +0800

    added pagenation

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,8 +2,8 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-   def index
-   @pins = Pin.all.order("created_at DESC")
+  def index
+   @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
